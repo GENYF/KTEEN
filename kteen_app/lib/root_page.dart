1b701d8c7cb2f5573999f0303d62b551b92a60d8
Yeongbeom Song <gnyf0915@gmail.com>
2022-05-25 15:22:47 +0900

    Refactor: Remove Unused Import

--- kteen_app/lib/root_page.dart
+++ kteen_app/lib/root_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:kteen_app/tab_page.dart';
 import 'package:kteen_app/login_page.dart';
-import 'package:kteen_app/utils/user_information.dart';
 
 class RootPage extends StatelessWidget {
   @override
@@ -12,7 +11,6 @@ class RootPage extends StatelessWidget {
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (BuildContext context, AsyncSnapshot snapshot) {
           if (snapshot.hasData) {
-
             return TabPage();
           }
           else{
